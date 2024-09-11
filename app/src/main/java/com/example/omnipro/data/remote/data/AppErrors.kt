4711408d1b149b29e43192b12e7e5da9c9115fab
Diego Casas <diego.casas@wgu.edu>
2024-09-11 12:49:00 -0600

    Adding strings

--- app/src/main/java/com/example/omnipro/data/remote/data/AppErrors.kt
+++ app/src/main/java/com/example/omnipro/data/remote/data/AppErrors.kt
@@ -3,8 +3,8 @@ package com.example.omnipro.data.remote.data
 import com.example.omnipro.R
 
 enum class AppErrors(val messageStringResource: Int) {
-    COMMUNICATION_SERVER_ERROR(messageStringResource = R.string.app_name),
-    UNABLE_COMPLETELY_PROCESS_QUERY(messageStringResource = R.string.app_name),
-    NO_CACHE_DATA(messageStringResource = R.string.app_name),
-    UNKNOWN_ERROR(messageStringResource = R.string.app_name)
+    COMMUNICATION_SERVER_ERROR(messageStringResource = R.string.server_error),
+    UNABLE_COMPLETELY_PROCESS_QUERY(messageStringResource = R.string.process_query_error),
+    NO_CACHE_DATA(messageStringResource = R.string.no_cache_data_error),
+    UNKNOWN_ERROR(messageStringResource = R.string.unknown_error)
 }
--- app/src/main/java/com/example/omnipro/presentation/characterlist/components/CharacterItemComponent.kt
+++ app/src/main/java/com/example/omnipro/presentation/characterlist/components/CharacterItemComponent.kt
@@ -3,19 +3,21 @@ package com.example.omnipro.presentation.characterlist.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
@@ -24,7 +26,7 @@ import com.example.omnipro.R
 import com.example.omnipro.domain.data.CharacterModel
 import com.example.omnipro.presentation.navigation.NavRoutes
 import com.example.omnipro.presentation.theme.DIMEN_240
-import com.example.omnipro.presentation.theme.DIMEN_32
+import com.example.omnipro.presentation.theme.DIMEN_4
 import com.example.omnipro.presentation.theme.DIMEN_8
 import com.example.omnipro.presentation.theme.LocalFont
 
@@ -53,19 +55,35 @@ fun CharacterItemComponent(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(DIMEN_32)
                 .clip(shape = RoundedCornerShape(bottomEnd = DIMEN_8, bottomStart = DIMEN_8))
                 .background(color = Color.DarkGray),
-            contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = characterModel.name,
-                style = TextStyle(
-                    fontStyle = FontStyle.Normal,
-                    fontFamily = LocalFont.current.semiBold,
-                    color = Color.White
+            Column(modifier = Modifier.padding(vertical = DIMEN_4, horizontal = DIMEN_4)) {
+                Text(
+                    text = stringResource(R.string.character_name, characterModel.name),
+                    style = TextStyle(
+                        fontStyle = FontStyle.Normal,
+                        fontFamily = LocalFont.current.semiBold,
+                        color = Color.White
+                    )
                 )
-            )
+                Text(
+                    text = stringResource(R.string.character_status, characterModel.status),
+                    style = TextStyle(
+                        fontStyle = FontStyle.Normal,
+                        fontFamily = LocalFont.current.semiBold,
+                        color = Color.White
+                    )
+                )
+                Text(
+                    text = stringResource(R.string.character_specie, characterModel.species),
+                    style = TextStyle(
+                        fontStyle = FontStyle.Normal,
+                        fontFamily = LocalFont.current.semiBold,
+                        color = Color.White
+                    )
+                )
+            }
         }
     }
 }
